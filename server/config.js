b81f7b0b6f8c77e7ddc6bb77b24c10860724461f
Pierre-Marie Dartus <p.dartus@salesforce.com>
2016-10-11 22:15:18 +0200

    Will probably fix CI

--- server/config.js
+++ server/config.js
@@ -3,20 +3,28 @@
 const fs = require('fs');
 const path = require('path');
 
-const CERT_DIR = process.env.DOCKER_CERT_PATH;
-if (!CERT_DIR) {
-  throw new Error('CERT_DIR env variable is needed to connect to docker');
-}
-
-const dockerConfig = {
-  protocol: 'https',
-  host: process.env.DOCKER_HOSTNAME || '192.168.99.100',
-  port: process.env.DOCKER_PORT || 2376,
-  checkServerIdentity: false,
-  ca: fs.readFileSync(`${CERT_DIR}/ca.pem`),
-  cert: fs.readFileSync(`${CERT_DIR}/cert.pem`),
-  key: fs.readFileSync(`${CERT_DIR}/key.pem`),
+// Check for default config
+let dockerConfig = {
+  socketPath: '/var/run/docker.sock',
 };
+
+// Check for docker machine running
+if (process.env.DOCKER_MACHINE_NAME) {
+  const CERT_DIR = process.env.DOCKER_CERT_PATH;
+  if (!CERT_DIR) {
+    throw new Error('CERT_DIR env variable is needed to connect to docker');
+  }
+
+  dockerConfig = {
+    protocol: 'https',
+    host: process.env.DOCKER_HOSTNAME || '192.168.99.100',
+    port: process.env.DOCKER_PORT || 2376,
+    checkServerIdentity: false,
+    ca: fs.readFileSync(`${CERT_DIR}/ca.pem`),
+    cert: fs.readFileSync(`${CERT_DIR}/cert.pem`),
+    key: fs.readFileSync(`${CERT_DIR}/key.pem`),
+  };
+}
 
 module.exports = {
   PORT: process.env.PORT || 3000,
